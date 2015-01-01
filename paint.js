4051d5ec8559ee38fb316d5ca6c12e64cfd3df62
chriscauley <chris@lablackey.com>
2015-01-01 17:29:42 -0500

    adding alpha

--- paint.js
+++ paint.js
@@ -9,7 +9,7 @@ function reset_canvas() {
 
 var mouse_down = false;
 
-var active_color, active_size;
+var active_color, active_size, alpha, current_action;
 var background_color = "white";
 
 var color_picker = document.getElementById("color_picker");
@@ -22,6 +22,11 @@ function setSize() { active_size = size_picker.value; }
 setSize();
 size_picker.addEventListener("change",setSize.bind(size_picker));
 
+var alpha_picker = document.getElementById("alpha_picker");
+var alpha_display = document.getElementById("alpha_display");
+function setAlpha() { alpha = alpha_picker.value; alpha_display.innerHTML = alpha; }
+setAlpha();
+//alpha_picker.addEventListener("change",setAlpha.bind(alpha_picker));
 
 var canvas = document.getElementById('paint');
 canvas.setAttribute('width', WIDTH);
@@ -29,16 +34,20 @@ canvas.setAttribute('height', HEIGHT);
 context = canvas.getContext("2d");
 
 function canvas_click(e) {
-  var mouseX = e.pageX - this.offsetLeft;
-  var mouseY = e.pageY - this.offsetTop;
   mouse_down = true;
-  addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-  redraw();
+  current_action = { coords: [], color: active_color, size: active_size, alpha: alpha };
+  actions.push(current_action);
+  current_action.canvas = document.createElement("canvas");
+  current_action.canvas.setAttribute('width', WIDTH);
+  current_action.canvas.setAttribute('height', HEIGHT);
+  current_action.context = current_action.canvas.getContext("2d");
+  canvas_move(e);
 }
 
 function canvas_move(e) {
   if(mouse_down){
-    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+    current_action.coords.push([e.layerX,e.layerY]);
+    brush(current_action);
     redraw();
   }
 };
@@ -48,32 +57,34 @@ canvas.addEventListener("mousemove",canvas_move.bind(canvas));
 canvas.addEventListener("mouseup",function() { mouse_down = false; })
 canvas.addEventListener("mouseout",function() { mouse_down = false; })
 
-function addClick(x, y, dragging) {
-  actions.push({x:x,y:y,dragging:dragging,color:active_color,size:active_size})
+function brush(action) {
+  var context = action.context;
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+  context.lineJoin = "round";
+  context.strokeStyle = action.color;
+  if (action.tool == "eraser") {
+    context.strokeStyle = background_color;
+  }
+  var last = [action.coords[0][0]-1,action.coords[0][1]];
+  for (var ai=0; ai<action.coords.length; ai++) {
+    context.beginPath();
+    context.moveTo(last[0],last[1]);
+    xy = action.coords[ai];
+    context.lineTo(xy[0],xy[1]);
+    context.closePath();
+    context.lineWidth = action.size;
+    context.stroke();
+    context.closePath();
+    last = xy;
+  }
 }
 
 function redraw() {
   context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
-
-  context.lineJoin = "round";
-
-  var last;
   for (var i=0; i < actions.length; i++) {
     var action = actions[i];
-    context.beginPath();
-    if (action.dragging && last) {
-      context.moveTo(last.x, last.y);
-    } else {
-      context.moveTo(action.x-1, action.y);
-    }
-    context.lineTo(action.x, action.y);
-    context.closePath();
-    context.strokeStyle = action.color;
-    if (action.tool == "eraser") {
-      context.strokeStyle = background_color;
-    }
-    context.lineWidth = action.size;
-    context.stroke();
-    var last = action;
+    context.setAlpha(action.alpha);
+    if (action.tool == "eraser") { context.setAlpha(1); }
+    context.drawImage(action.canvas,0,0);
   }
 }
