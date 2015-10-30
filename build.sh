acb159d401c326ecf1859b351c3b9b5534b1edfd
chriscauley <chris@lablackey.com>
2015-10-29 21:55:20 -0400

    adding bower back in along with unrest/modal.tag

---
+++ build.sh
@@ -0,0 +1,5 @@
+# A small bash script to move all files to the live directory
+
+cp index.html /home/chriscauley/public/paint
+cp -r .static /home/chriscauley/public/paint
+cp -r tags /home/chriscauley/public/paint
